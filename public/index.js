f9947c0dda3b3bce62107eb05fcfbd1ab007da49
Thien Trinh <takatathien@gmail.com>
2022-08-07 14:57:02 -0700

    Added gravity

--- public/index.js
+++ public/index.js
@@ -6,6 +6,7 @@ canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
+const gravity = 0.2
 class Sprite {
 	constructor({position, velocity}) {
 		this.position = position
@@ -20,6 +21,7 @@ class Sprite {
 	
 	update() {
 		this.draw()
+		this.velocity.y += gravity
 		this.position.y += this.velocity.y
 		
 		if (this.position.y + this.height + this.velocity.y >= canvas.height) {
@@ -35,7 +37,7 @@ const player = new Sprite({
 	},
 	velocity: {
 		x: 0,
-		y: 10
+		y: 0
 	}
 })
 

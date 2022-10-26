a5a701178a913427bf9b58817ab29752828818f7
Thien Trinh <takatathien@gmail.com>
2022-10-25 23:00:50 -0700

    finished basic movements.

--- public/index.js
+++ public/index.js
@@ -6,12 +6,14 @@ canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
-const gravity = 0.2
+const gravity = 0.7
+
 class Sprite {
 	constructor({position, velocity}) {
 		this.position = position
 		this.velocity = velocity
 		this.height = 150
+		this.lastKey
 	}
 	
 	draw() {
@@ -62,9 +64,14 @@ const keys = {
 	},
 	d: {
 		pressed: false
+	},
+	ArrowRight: {
+		pressed: false
+	},
+	ArrowLeft: {
+		pressed: false
 	}
 }
-let lastKey
 
 function animate() {
 	window.requestAnimationFrame(animate)
@@ -74,28 +81,50 @@ function animate() {
 	enemy.update()
 	
 	player.velocity.x = 0
+	enemy.velocity.x = 0
 	
-	if (keys.a.pressed && lastKey === 'a') {
-		player.velocity.x = -1
-	} else if (keys.d.pressed && lastKey === 'd') {
-		player.velocity.x = 1
+	// player movement
+	if (keys.a.pressed && player.lastKey === 'a') {
+		player.velocity.x = -5
+	} else if (keys.d.pressed && player.lastKey === 'd') {
+		player.velocity.x = 5
+	}
+	
+	// enemy movement
+	if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+		enemy.velocity.x = -5
+	} else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+		enemy.velocity.x = 5
 	}
 }
 
 animate()
 
 window.addEventListener('keydown', (event) => {
+	console.log(event.key)
 	switch (event.key) {
 		case 'd':
 			keys.d.pressed = true
-			lastKey = 'd'
+			player.lastKey = 'd'
 			break
 		case 'a':
 			keys.a.pressed = true
-			lastKey = 'a'
+			player.lastKey = 'a'
 			break
 		case 'w':
-			player.velocity.y = -10
+			player.velocity.y = -20
+			break
+			
+		case 'ArrowRight':
+			keys.ArrowRight.pressed = true
+			enemy.lastKey = 'ArrowRight'
+			break
+		case 'ArrowLeft':
+			keys.ArrowLeft.pressed = true
+			enemy.lastKey = 'ArrowLeft'
+			break
+		case 'ArrowUp':
+			enemy.velocity.y = -20
 			break
 	}
 	console.log(event.key)
@@ -108,6 +137,16 @@ window.addEventListener('keyup', (event) => {
 			break
 		case 'a':
 			keys.a.pressed = false
+			break
+	}
+	
+	// enemy keys
+	switch (event.key) {
+		case 'ArrowRight':
+			keys.ArrowRight.pressed = false
+			break
+		case 'ArrowLeft':
+			keys.ArrowLeft.pressed = false
 			break
 	}
 	console.log(event.key)
